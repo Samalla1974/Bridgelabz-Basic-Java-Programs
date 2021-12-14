a299ff0898b53fbf2311a597e2e3709eeaa69a45
Samalla1974 <94819969+Samalla1974@users.noreply.github.com>
2021-12-15 00:02:39 +0530

    Add files via upload

---
+++ Stopwatcher.java
@@ -0,0 +1,26 @@
+package Basic_Logical_Programs;
+import java.util.*;  
+
+public class Stopwatcher {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in); 
+		int start= sc.nextInt();
+		int stop= sc.nextInt();
+		if (start == 0)
+		{
+			long a = System.currentTimeMillis();
+		}
+		else if (stop == 0)
+		{
+			long b = System.currentTimeMillis();
+		}
+		else
+		{
+			System.out.println("enter 0 to start");
+		}
+		long diff = stop - start;
+		System.out.println("The difference between start and stop is " + diff);
+	}
+
+}
